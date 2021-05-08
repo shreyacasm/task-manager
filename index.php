5dc8a7bc2a4438651028f72988a033c235945540
shreya_s <shreya.suroliya@createbytes.com>
2021-05-08 10:46:29 +0530

    index file

---
+++ index.php
@@ -0,0 +1,46 @@
+<html>
+    <head>
+        <title>Task Manager</title>
+    </head>
+    <body>
+        <h1>Task Manager</h1>
+        <!-- Menu starts here -->
+        <div class="menu">    
+            <a href="index.php">Home</a>
+            
+            <a href="#">To Do</a>
+            <a href="#">Doing</a>
+            <a href="#">Done</a>
+
+            <a href="manage-list.php">Manage List</a>
+        </div>
+        <!-- Menu ends here -->
+        
+        <!-- Task Starts here  -->
+        <div class="all-task">
+            <a href="#">Add Task</a>
+            <table>
+                <tr>
+                    <th>Serial No</th>
+                    <th>Task Name</th>
+                    <th>Priority</th>
+                    <th>Deadline</th>
+                    <th>Action</th>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>Design a website</td>
+                    <td>Medium</td>
+                    <td>23/09/2021</td>
+                    <td>
+                        <a href="#">Update</a>
+                        <a href="#">Delete</a>
+                    </td>
+                </tr>
+            </table>
+
+        </div>
+        <!-- Task Starts here  -->
+
+    </body>
+</html>
