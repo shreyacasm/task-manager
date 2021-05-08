eb9436fdd0b3e79adbbc7b2d9a34970367a2abf3
shreya_s <shreya.suroliya@createbytes.com>
2021-05-08 12:41:04 +0530

    home managelist

---
+++ manage-list.php
@@ -0,0 +1,42 @@
+<html>
+    <head>
+        <title>Task Manager</title>
+    </head>
+    <body>
+        <h1>Task Manager</h1>
+        <a href="index.php">Home</a>
+        <h3>Manage List Page</h3>
+
+        <!-- table to display list starts here -->
+        <div class="all-lists">
+            <a href="add-list.php">Add list</a>
+            <table>
+                <tr>
+                    <th>S No.</th>
+                    <th>List Name</th>
+                    <th>Action</th>
+                </tr>
+
+                <tr>
+                    <td>1</td>
+                    <td>To Do</td>
+                    <td>
+                        <a href="#">Update</a>
+                        <a href="#">Delete</a>
+                    </td>
+                </tr>
+                <tr>
+                    <td>2</td>
+                    <td>Doing</td>
+                    <td>
+                        <a href="#">Update</a>
+                        <a href="#">Delete</a>
+                    </td>
+                </tr>
+            </table>
+
+        </div>
+        
+        <!-- table to display list ends here -->
+    </body>
+</html>
