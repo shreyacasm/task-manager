fc814cb35a7d724565b7b068b6bb2fbdc1886c25
shreya_s <shreya.suroliya@createbytes.com>
2021-05-24 01:04:38 +0530

    css n cdn added

--- list-task.php
+++ list-task.php
@@ -7,6 +7,8 @@
 <html>
     <head>
         <title>Task Manager</title>
+        <link rel="stylesheet" href="<?php echo SITEURL;?>css/style.css">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
     </head>
 
     <body>
